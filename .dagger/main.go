9d2625215e0f11a3d33ff33b29a3a0f46c4b8e46
Matias Pan <gh@matiaspan.dev>
2025-04-25 11:38:44 -0300

    Bump dagger version
    Signed-off-by: Matias Pan <gh@matiaspan.dev>

--- .dagger/main.go
+++ .dagger/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 const (
-	golangciLintVersion = "1.60.3"
-	goVersion           = "1.23"
-	alpineVersion       = "3.20"
+	golangciLintVersion = "2.1-alpine@sha256:eff222d3ac17f7e2a12dbe757cb33c2dc7899cd5bfae4432594e558a1e1e0228"
+	goVersion           = "1.24.2-alpine3.21@sha256:7772cb5322baa875edd74705556d08f0eeca7b9c4b5367754ce3f2f00041ccee"
+	alpineVersion       = "3.21@sha256:a8560b36e8b8210634f77d9f7f9efd7ffa463e380b75e2e74aff4511df3ef88c"
 )
 
 type DaggerRegistry struct {
@@ -27,7 +27,7 @@ func New(
 
 func (m *DaggerRegistry) Lint(ctx context.Context) *dagger.Container {
 	return dag.Container().
-		From(fmt.Sprintf("golangci/golangci-lint:v%s-alpine", golangciLintVersion)).
+		From(fmt.Sprintf("golangci/golangci-lint:v%s", golangciLintVersion)).
 		WithMountedCache("/go/pkg/mod", dag.CacheVolume("registry-gomod")).
 		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
 		WithDirectory("/app", m.Source).
@@ -53,7 +53,7 @@ func (m *DaggerRegistry) Build(ctx context.Context) *dagger.Container {
 
 func (m *DaggerRegistry) baseContainer(ctx context.Context) *dagger.Container {
 	return dag.Container().
-		From(fmt.Sprintf("golang:%s-alpine%s", goVersion, alpineVersion)).
+		From(fmt.Sprintf("golang:%s", goVersion)).
 		WithMountedCache("/root/.cache/go-build", dag.CacheVolume("registry-go-build")).
 		WithEnvVariable("GOCACHE", "/root/.cache/go-build").
 		WithMountedCache("/go/pkg/mod", dag.CacheVolume("registry-gomod")).
